f53bacbe0d093f1a0c2bdaa33c15bfff9664159a
Apheliont <apheliont@gmail.com>
2017-10-12 23:27:40 +0300

    minified

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const nodeEnv = process.env.NODE_ENV || 'production';
 
 module.exports = {
   devtool: 'source-map',
@@ -6,5 +8,26 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: { comments: false },
+      sourceMap: true
+    }),
+    new webpack.DefinePlugin({
+      'proccess.env': { NODE_ENV: JSON.stringify(nodeEnv)}
+    })
+  ]
 };
